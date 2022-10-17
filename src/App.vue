f3ef545fee516ce090ba8710cded22dbee1e2a29
黄涛 <2231453497@qq.com>
2022-10-17 20:28:18 +0800

    feat: add naive-ui

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,31 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+import { NConfigProvider, darkTheme, zhCN, dateZhCN } from 'naive-ui'
+import type { GlobalTheme } from 'naive-ui'
 import { storeToRefs } from 'pinia'
 import { globalStore } from '@/store/modules/global'
+
+const theme = ref<GlobalTheme | null>(null)
 
 const store = globalStore()
 const { changeUserInfo } = store
 const { userInfo } = storeToRefs(store)
+const inputValue = ref(0)
+const timestamp = ref(1183135260000)
 </script>
 
 <template>
-  <span>姓名: {{ userInfo.name }}</span>
-  <span>年龄: {{ userInfo.age }}</span>
-  <span>性别: {{ userInfo.sex }}</span>
-  <button @click="changeUserInfo">修改姓名</button>
+  <n-config-provider :theme="theme" :locale="zhCN" :date-locale="dateZhCN">
+    <span>姓名: {{ userInfo.name }}</span>
+    <span>年龄: {{ userInfo.age }}</span>
+    <span>性别: {{ userInfo.sex }}</span>
+    <n-button type="tertiary" @click="changeUserInfo"> 修改姓名 </n-button>
+    <n-tag type="success" @click="theme = null">亮色</n-tag>
+    <n-tag type="success" @click="theme = darkTheme">暗色</n-tag>
+    <n-input-number v-model:value="inputValue" clearable />
+    <n-date-picker v-model:value="timestamp" type="date" />
+    <!-- <n-color-picker size="small" /> -->
+  </n-config-provider>
 </template>
 
 <style scoped>
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,26 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+		vue(),
+		// 自动引入组件库
+		AutoImport({
+			resolvers: [NaiveUiResolver()],
+			dts: 'src/types/auto-imports.d.ts',
+		}),
+		Components({
+			dirs: [],
+			resolvers: [NaiveUiResolver()],
+			directoryAsNamespace: true,
+			dts: 'src/types/components.d.ts',
+		}),
+  ],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src')
